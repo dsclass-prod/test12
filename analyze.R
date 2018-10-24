274ad9b55300ced9e2f229fce84563f5f573a6ca
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-24 17:28:58 -0400

    Saving analyze.R

--- analyze.R
+++ analyze.R
@@ -3,4 +3,4 @@ library(ggplot2)
 theme_set(theme_classic())
 
 g <- ggplot(df, aes(df$Airline.Status))
-g <- g + geom_bar(aes(fill=df$Type.of.Travel), width = 0.5) + theme(axis.text.x = element_text(angle=65, vjust=0.6)) + labs(title="Histogram on Categorical Variable", subtitle="Airline status across Type of Travel")
+img <- g + geom_bar(aes(fill=df$Type.of.Travel), width = 0.5) + theme(axis.text.x = element_text(angle=65, vjust=0.6)) + labs(title="Histogram on Categorical Variable", subtitle="Airline status across Type of Travel")
